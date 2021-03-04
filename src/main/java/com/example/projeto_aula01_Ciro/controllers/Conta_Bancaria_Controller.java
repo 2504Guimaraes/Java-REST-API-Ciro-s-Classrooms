dadb8d50fe379827acbd547a9e08dbdbb0c58cc8
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-03-04 18:09:21 -0300

    adding Get and Post User Account methods

--- src/main/java/com/example/projeto_aula01_Ciro/controllers/Conta_Bancaria_Controller.java
+++ src/main/java/com/example/projeto_aula01_Ciro/controllers/Conta_Bancaria_Controller.java
@@ -3,10 +3,9 @@ package com.example.projeto_aula01_Ciro.controllers;
 import com.example.projeto_aula01_Ciro.models.Conta_Bancaria;
 import com.example.projeto_aula01_Ciro.services.Conta_Bancaria_Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,17 @@ public class Conta_Bancaria_Controller {
         return ResponseEntity.ok(servico.acharTodasAsContas());
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> get(@PathVariable("id") Long id) {
+        Conta_Bancaria _conta = servico.acharConta(id);
+        if (_conta != null)
+            return ResponseEntity.ok(_conta);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 
+    @PostMapping
+    public ResponseEntity<Conta_Bancaria> post(@RequestBody Conta_Bancaria novaConta) {
+        servico.criarNovaConta(novaConta);
+        return ResponseEntity.ok(novaConta);
+    }
 }
