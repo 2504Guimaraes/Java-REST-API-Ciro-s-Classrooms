09b7cb648dea70736b6d23961ee38de1fbcdbc18
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-03-04 15:34:25 -0300

    Adding 'getAllAccounts' method

--- src/main/java/com/example/projeto_aula01_Ciro/controllers/Conta_Bancaria_Controller.java
+++ src/main/java/com/example/projeto_aula01_Ciro/controllers/Conta_Bancaria_Controller.java
@@ -1,9 +1,14 @@
 package com.example.projeto_aula01_Ciro.controllers;
 
+import com.example.projeto_aula01_Ciro.models.Conta_Bancaria;
 import com.example.projeto_aula01_Ciro.services.Conta_Bancaria_Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/contas-bancarias")
@@ -11,6 +16,11 @@ public class Conta_Bancaria_Controller {
 
     @Autowired
     private Conta_Bancaria_Service servico;
+
+    @GetMapping
+    public ResponseEntity<List<Conta_Bancaria>> pegarTodasAsContas() {
+        return ResponseEntity.ok(servico.acharTodasAsContas());
+    }
 
 
 }
