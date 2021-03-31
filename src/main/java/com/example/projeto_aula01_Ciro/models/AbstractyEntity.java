7e312a20ec56592ddcd965aaac6b313444ea2757
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-03-31 16:18:02 -0300

    Adding superclass (MappedSuperClass) that will be inherited by other data entities

---
+++ src/main/java/com/example/projeto_aula01_Ciro/models/AbstractyEntity.java
@@ -0,0 +1,38 @@
+package com.example.projeto_aula01_Ciro.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractyEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractyEntity that = (AbstractyEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
