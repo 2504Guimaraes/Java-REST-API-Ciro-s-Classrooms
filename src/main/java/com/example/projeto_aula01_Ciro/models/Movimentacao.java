51092cb67a2c7214155a39aa64db7b74c59ff1ae
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-04-08 17:48:06 -0300

    adding to class new attribute conta and its getters and setters

--- src/main/java/com/example/projeto_aula01_Ciro/models/Movimentacao.java
+++ src/main/java/com/example/projeto_aula01_Ciro/models/Movimentacao.java
@@ -1,6 +1,8 @@
 package com.example.projeto_aula01_Ciro.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 import java.util.Calendar;
@@ -25,6 +27,19 @@ public class Movimentacao extends AbstractyEntity {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd")
     @Column(name = "dt_movimentacao")
     private Calendar data;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Conta_Bancaria conta;
+
+    @JsonIgnore
+    public Conta_Bancaria getConta() {
+        return conta;
+    }
+
+    @JsonProperty
+    public void setConta(Conta_Bancaria conta) {
+        this.conta = conta;
+    }
 
     public Movimentacao () {
         super();
