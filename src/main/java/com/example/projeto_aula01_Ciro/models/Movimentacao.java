64b0c7c7d93370557a87f3001f0814ba2320759c
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-04-08 04:02:34 -0300

    class Movimentacao completed

--- src/main/java/com/example/projeto_aula01_Ciro/models/Movimentacao.java
+++ src/main/java/com/example/projeto_aula01_Ciro/models/Movimentacao.java
@@ -1,12 +1,65 @@
 package com.example.projeto_aula01_Ciro.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Calendar;
 
 public class Movimentacao extends AbstractyEntity {
     private static final long serialVersionUID = 1L;
 
+    @Column(name = "vl_movimentacao")
     private Float valor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "nm_tipo_movimentacao")
     private TipoMovimentacao tipo;
+
+    @Column(name = "ds_descricao", length = 100)
     private String descricao;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd")
+    @Column(name = "dt_movimentacao")
     private Calendar data;
+
+    public Movimentacao () {
+        super();
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+
+    public TipoMovimentacao getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoMovimentacao tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Calendar getData() {
+        return data;
+    }
+
+    public void setData(Calendar data) {
+        this.data = data;
+    }
 }
